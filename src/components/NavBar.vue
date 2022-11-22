0e301852b3f2c50c0c4ef6d6b161525096f104c0
Theron Joe <theronj@joetwebdev.io>
2022-11-21 23:41:34 -0700

    moved pictures, refactored navbar, now has reactivity between astro loading. wip.

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,48 @@
 <script lang="ts" setup>
-import Logo from '../../../imgs/mnts.png';
+import { onMounted, nextTick, ref } from 'vue';
+import Logo from '../../src/imgs/mnts.png';
+
+let navItems =  ref([
+	{ name: 'Home', link: '/', active: true },
+	{ name: 'About', link: '/about', active: false },
+	{ name: 'Blog', link: '/blog', active: false },
+	{ name: 'Resume', link: '#resume', active: false },
+	{ name: 'Projects', link: '#projects', active: false },
+	{ name: 'Contact', link: '#contact', active: false },
+])
+
+onMounted(() => {
+	nextTick(() => {
+		setNav()
+	})
+})
+
+ async function setNav() {
+	console.log(window.location.pathname)
+	navItems.value.forEach((nav) => {
+		if (window.location.pathname == nav.link) {
+			nav.active = true
+		} else {
+			nav.active = false
+		}
+	})
+	console.log(navItems)
+}
 </script>
 
 <template id="nav-bar">
-	<nav class="flex justify-between items-center px-6 py-4 border bg-gray-300">
+	<nav class="flex justify-between min-w-full items-center px-6 py-4 border bg-gray-500">
 		<div id="logo" class="mx-4">
 			<div class="border rounded-lg">
-				<img id="" :src="Logo" alt="" class="w-10 h-10">
+				<img id="" :src="Logo" alt="" class="w-12 h-12">
 			</div>
 		</div>
 		<div>
-			<ul class="flex space-x-4 text-xl">
-				<li>
-					<a href="/" class="hover:bg-gray-400 hover:text-white rounded px-4 py-2">Home</a>
-				</li>
-				<li>
-					<a href="" class="hover:bg-gray-400 hover:text-white rounded px-4 py-2">About</a>
-				</li>
-				<li>
-					<a href="/blog" class="hover:bg-gray-400 hover:text-white rounded px-4 py-2">Blog</a>
-				</li>
-				<li>
-					<a href="" class="hover:bg-gray-400 hover:text-white rounded px-4 py-2">Projects</a>
-				</li>
-				<li>
-					<a href="" class="hover:bg-gray-400 hover:text-white rounded px-4 py-2">Contact</a>
+			<ul class="md:flex justify-self-end items-center text-gray-100 hidden">
+				<li v-for="nav in navItems" :key="nav.name" :class="['mx-2 font-black', { 'border-b-4': nav.active }]">
+					<a :href="nav.link">
+						{{ nav.name }}
+					</a>
 				</li>
 			</ul>
 		</div>
