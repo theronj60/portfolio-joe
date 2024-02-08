2c806c4c6dfe9741dbcf9f2ae193492756bafd2c
Theron Joe <theronj@joetwebdev.io>
2024-02-08 00:37:44 -0700

    added better use of the posthog snippet, updated colorscheme, removed all images, updated styling for a more readable streamline code, added a new footer component, added some projects and updated the resume portion. WIP

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+
+<template id="footer">
+	<footer class="flex justify-center py-6 text-sm text-gray-600 bg-gray-900">
+		<p>
+			© 2024 Theron Joe.
+			<a href="https://github.com/theronj60/portfolio-joe" target="_blank" class="hover:text-blue-400 hover:underline hover:underline-offset-2">This website is open source.</a>
+		</p>
+	</footer>
+</template>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -35,36 +35,38 @@ function toggleNav() {
 </script>
 
 <template id="nav-bar">
-	<nav class="flex justify-between min-w-full items-center px-6 py-4 bg-gray-800 text-gray-100">
-		<div id="logo" class="mx-4">
-			<div class="border rounded-lg">
-				<img id="" :src="Logo" alt="" class="w-12 h-12">
+	<header>
+		<nav class="flex justify-between min-w-full items-center px-6 py-4 bg-gray-900 text-gray-100">
+			<div id="logo" class="mx-4">
+				<div class="border rounded-lg">
+					<img id="" :src="Logo" alt="" class="w-12 h-12">
+				</div>
 			</div>
-		</div>
-		<div class="flex md:hidden">
-			<button type="button" @click="toggleNav" class="text-white">
-				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
-				  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-				</svg>
-			</button>
-		</div>
-		<div class="md:flex hidden">
-			<ul class="md:flex justify-self-end items-center text-gray-100 hidden">
-				<li v-for="nav in navItems" :key="nav.name" :class="['mx-2 font-black', { 'border-b-4': nav.active }]">
-					<a :href="nav.link">
+			<div class="flex md:hidden">
+				<button type="button" @click="toggleNav" class="text-white">
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-9 h-9">
+					  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+					</svg>
+				</button>
+			</div>
+			<div class="md:flex hidden">
+				<ul class="md:flex justify-self-end items-center text-gray-100 hidden">
+					<li v-for="nav in navItems" :key="nav.name" :class="['mx-2 font-black', { 'border-b-4': nav.active }]">
+						<a :href="nav.link">
+							{{ nav.name }}
+						</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+		<div v-if="mobileNav" class="w-full bg-gray-800 fixed top-22">
+			<ul>
+				<li v-for="nav in navItems" :key="nav.name" :class="['flex text-center text-gray-100 font-black', { 'bg-gray-600': nav.active }]">
+					<a :href="nav.link" class="w-full text-center py-4">
 						{{ nav.name }}
 					</a>
 				</li>
 			</ul>
 		</div>
-	</nav>
-	<div v-if="mobileNav" class="w-full bg-gray-800 fixed top-22">
-		<ul>
-			<li v-for="nav in navItems" :key="nav.name" :class="['flex text-center text-gray-100 font-black', { 'bg-gray-600': nav.active }]">
-				<a :href="nav.link" class="w-full text-center py-4">
-					{{ nav.name }}
-				</a>
-			</li>
-		</ul>
-	</div>
+	</header>
 </template>
